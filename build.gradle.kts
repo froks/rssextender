720827a9d7cd507d818d7c066e5d400ab6087f32
Florian Roks <froks@users.noreply.github.com>
2021-04-14 17:40:10 +0200

    put junit jupiter version in val

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,12 @@ repositories {
 
 val ktorVersion = "1.5.3"
 
+val junitJupiterVersion = "5.7.1"
+
 dependencies {
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-html-builder:$ktorVersion")
